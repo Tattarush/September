4db7277b30745854753274e634ecd4cfc457b595
tattarush <tattarush@gmail.com>
2025-09-03 16:50:55 +0300

    задача с ТГ

---
+++ src/main/java/org/example/workDir030925/Main01.java
@@ -0,0 +1,15 @@
+package org.example.workDir030925;
+
+public class Main01 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = a++;
+        int c = -a;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
